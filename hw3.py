d4b3a84c801ca7e06e42f20840f4e5219fa5ee5c
Willie <willie0507@gmail.com>
2018-05-20 02:49:08 +0800

    plot curve, optimize data load

--- hw3.py
+++ hw3.py
@@ -10,26 +10,24 @@ from tqdm import tqdm
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 INPUT_SIZE = [224, 224, 3]
 N_CLASSES = 120
 LEARNING_RATE = 2e-5
-EPOCHS = 1
+EPOCHS = 50
 BATCH_SIZE = 10
 LOAD_PRETRAIN = True
+EPOCHS_LIST = []
+LOSS_LIST = []
+ACC_LIST = []
 
 
-def load_images(path, mode='id', half=False, pos=None):
+def image_gen(path, start, end):
     files = os.listdir(path)
     files.sort()
-    if mode == 'id':
-        return files
 
-    if half and pos == 'front':
-        files = files[:int(len(files) / 2)]
-    elif half and pos == 'back':
-        files = files[int(len(files) / 2):]
-
+    files = files[start:end]
     data = []
     for file in files:
         img = io.imread(path + "/" + file)
@@ -40,6 +38,12 @@ def load_images(path, mode='id', half=False, pos=None):
     data = np.array(data)
 
     return data
+
+
+def load_images_id(path):
+    files = os.listdir(path)
+    files.sort()
+    return files
 
 
 def load_label(path, mode='class'):
@@ -75,14 +79,15 @@ def softmax(x):
 
 
 def train_eval(sess, x_data, y_label, batch_size, train_phase, is_eval, epoch=None):
-    n_sample = x_data.shape[0]
+    n_sample = len(os.listdir(x_data))
     n_batch = int((n_sample + batch_size - 1) / batch_size)
     tmp_loss, tmp_acc = 0, 0
     for batch in tqdm(range(n_batch)):
         start = batch * batch_size
         end = min(n_sample, start + batch_size)
+        train_data = image_gen(x_data, start, end)
         _, batch_loss, batch_acc = sess.run([train_op, loss, accuracy],
-                                            feed_dict={x: x_data[start:end], y: y_label[start:end],
+                                            feed_dict={x: train_data, y: y_label[start:end],
                                                        is_training: train_phase})
         tmp_loss += batch_loss * (end - start)
         tmp_acc += batch_acc * (end - start)
@@ -90,21 +95,23 @@ def train_eval(sess, x_data, y_label, batch_size, train_phase, is_eval, epoch=No
     tmp_acc /= n_sample
     if train_phase:
         print('\nepoch: {0}, loss: {1:.4f}, acc: {2:.4f}'.format(epoch + 1, tmp_loss, tmp_acc))
+        EPOCHS_LIST.append(epoch + 1), LOSS_LIST.append(tmp_loss), ACC_LIST.append(tmp_acc)
 
 
 def test_eval(sess, x_data, train_phase):
     batch_size = 1
-    n_sample = x_data.shape[0]
+    n_sample = len(os.listdir(x_data))
     n_batch = int((n_sample + batch_size - 1) / batch_size)
     tmp_pred = []
     log = []
     for batch in tqdm(range(n_batch)):
         start = batch * batch_size
         end = min(n_sample, start + batch_size)
-        tmp_logits = sess.run(logits, feed_dict={x: x_data[start:end], is_training: train_phase})
+        test_data = image_gen(x_data, start, end)
+        tmp_logits = sess.run(logits, feed_dict={x: test_data, is_training: train_phase})
         tmp = softmax(np.squeeze(tmp_logits))
         tmp_pred.append(tmp)
-    # tmp_pred = np.array(tmp_pred)
+        tmp_pred = np.array(tmp_pred)
 
     return tmp_pred
 
@@ -114,13 +121,19 @@ def convert_csv(image_id, label, result):
     df.to_csv(r"submission.csv")
 
 
-# data preprocess by yourself
+def plot(x, y, type):
+    plt.plot(x, y, label=type)
+    plt.ylabel(type)
+    plt.xlabel('epoch')
+    plt.legend([type], loc='upper left')
+    plt.savefig(type)
+    plt.clf()
+
+
 if __name__ == '__main__':
     start_time = time.time()
 
-    train_data = load_images(r"data/train", "n")
     train_label = load_label(r"data/labels.csv", "n")
-    # test_data = load_images(r"data/test", "r")
 
     x = tf.placeholder(dtype=tf.float32, shape=(None, INPUT_SIZE[0], INPUT_SIZE[1], INPUT_SIZE[2]), name='x')
     y = tf.placeholder(dtype=tf.float32, shape=(None, N_CLASSES), name='y')
@@ -140,8 +153,6 @@ if __name__ == '__main__':
     restore_variable = [var for var in tf.global_variables() if var.name.startswith('')]
 
     saver = tf.train.Saver()
-    # config = tf.ConfigProto()
-    # config.gpu_options.per_process_gpu_memory_fraction = 0.5
     with tf.Session() as sess:
         if LOAD_PRETRAIN:
             saver.restore(sess, r'model/pretrain/model.ckpt')
@@ -149,15 +160,13 @@ if __name__ == '__main__':
             sess.run(init)
 
         for i in tqdm(range(EPOCHS)):
-            train_eval(sess=sess, x_data=train_data, y_label=train_label,
+            train_eval(sess=sess, x_data=r"data/train", y_label=train_label,
                        batch_size=BATCH_SIZE, train_phase=True, is_eval=False, epoch=i)
         # saver.save(sess, 'model/model.ckpt')
-        del train_data
-        del train_label
-        ans = test_eval(sess=sess, x_data=load_images(r"data/test", "n", True, 'front'), train_phase=False)
-        ans.extend(test_eval(sess=sess, x_data=load_images(r"data/test", "n", True, 'back'), train_phase=False))
-    convert_csv(load_images(r"data/test"), load_label(r"data/labels.csv"), np.array(ans))
-
+        ans = test_eval(sess=sess, x_data=r"data/test", train_phase=False)
+    convert_csv(load_images_id(r"data/test"), load_label(r"data/labels.csv"), ans)
+    plot(EPOCHS_LIST, LOSS_LIST, 'loss')
+    plot(EPOCHS_LIST, ACC_LIST, 'acc')
 
     end_time = time.time()
     print("\nTime usage:", timedelta(seconds=int(round(end_time - start_time))))
